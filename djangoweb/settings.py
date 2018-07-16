9d7509b9181b237fe65093dd07eeb6a6e93823b7
asus <1016450320@qq.com>
2018-07-16 14:57:13 +0800

    更新

--- djangoweb/settings.py
+++ djangoweb/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'blog',
     'read_statistics',
     'comment',
+    'likes',
 ]
 
 MIDDLEWARE = [
@@ -140,9 +141,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 CKEDITOR_UPLOAD_PATH = 'upload/'
 
 CKEDITOR_CONFIGS = {
-    'default':{
-        'toolbar': 'FULL',
-    },
+    'default': {},
     'comment_ckeditor': {
         'toolbar': 'custom',
         'toolbar_custom': [
